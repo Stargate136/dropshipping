d6300143d155fc980ba84722dd040a30ec3801af
Christian Aucane <christianaucane1@gmail.com>
2023-04-11 08:05:03 +0200

    Refactorisation

---
+++ src/store/static/store/js/add_to_cart.js
@@ -0,0 +1,31 @@
+export function addEventsListenersQuantity(incrementButton, decrementButton, quantityInput) {
+
+    // Ajout de gestionnaires d'événements aux boutons
+    incrementButton.addEventListener("click", function(event) {
+        event.preventDefault();
+        // Conversion de la valeur en nombre et ajout de 1
+        let currentValue = parseInt(quantityInput.value);
+        quantityInput.value = currentValue + 1;
+    });
+
+    decrementButton.addEventListener("click", function(event) {
+        event.preventDefault();
+        // Conversion de la valeur en nombre et soustraction de 1
+        let currentValue = parseInt(quantityInput.value);
+        // Vérification que la valeur est supérieure à 1 pour éviter d'avoir des nombres négatifs
+        if (currentValue > 1) {
+        quantityInput.value = currentValue - 1;
+        }
+    });
+}
+
+export function addEventListenerAddToCart() {
+    // Ajout de gestionnaires d'événements aux boutons
+    addToCartLink.addEventListener("click", function(event) {
+        event.preventDefault();
+        const quantityValue = parseInt(quantityInput.value);
+        addToCartLink.href += `?quantity=${quantityValue}`;
+        window.location.href = addToCartLink.href;
+    })
+}
+
